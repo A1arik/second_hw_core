5c2429a17671ea2979ac522778bd09c772ae2984
A1arik <istlyaev@gmail.com>
2020-09-30 12:24:35 +0300

    first_commit

---
+++ soccer/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using soccer.Models;
+
+namespace soccer.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            Players.LoadAsync();
+            Teams.LoadAsync();
+        }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
